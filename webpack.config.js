9faab57526d79b466abe213c6e341e03ac2a62b2
Karina Espinola <k.espinolah@gmail.com>
2021-06-29 16:08:07 -0600

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+    entry: { 
+        index: path.resolve(__dirname, "src", "app.js") 
+    },
+    output: {
+        path: path.resolve(__dirname, "build")
+    },
+    watch: true
+}
