5f43a686b9d68d81a2c9f456a79c2c16fe54f302
Karina Espinola <k.espinolah@gmail.com>
2021-06-29 16:09:48 -0600

    Add js script to the site

--- functions.php
+++ functions.php
@@ -144,6 +144,7 @@ function bees_scripts() {
 	wp_style_add_data( 'bees-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'bees-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'bees-js', get_template_directory_uri() . '/build/app.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
