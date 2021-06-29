6f87e58aec9612aca982bc17b70e6a9e30128a08
Karina Espinola <k.espinolah@gmail.com>
2021-06-29 14:33:21 -0600

    Add clouds heading animation

--- template-parts/content-front_page.php
+++ template-parts/content-front_page.php
@@ -11,10 +11,13 @@
 
 <section class="bg-primary">
 	<div class="relative h-screen container md:mx-auto">
-		<div>
-			<img src="<?php echo get_template_directory_uri() . '/img/cloud-animation-1.svg' ?> " alt="">
+		<div class="clouds">
+			<img src="<?php echo get_template_directory_uri() . '/img/cloud.svg' ?>"  alt="">
+			<img src="<?php echo get_template_directory_uri() . '/img/cloud.svg' ?>"  alt="">
+			<img src="<?php echo get_template_directory_uri() . '/img/cloud.svg' ?>"  alt="">
+			<img src="<?php echo get_template_directory_uri() . '/img/cloud.svg' ?>"  alt="">
 		</div>
-		<div class="absolute bottom-2/4">
+		<div class="absolute bottom-2/4 z-10">
 			<h1 class="text-4xl font-bold uppercase border-b-8 border-black">We are listening...</h1>
 			<h2 class="mt-3">Don't hold back we are ready to turn your ideas into goals</h2>	
 		</div>
